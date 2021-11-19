733ed7e823e9cd241300b30143432af0d112a4fb
szherixov <s0p9n9k@gmail.com>
2021-11-19 15:02:22 +0200

    finaly

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const VIDEO_KEY = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  const time = JSON.stringify(data.seconds);
+  localStorage.setItem(VIDEO_KEY, time);
+  console.log('played the video!', time);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const saveTime = localStorage.getItem(VIDEO_KEY);
+
+player
+  .setCurrentTime(saveTime)
+  .then(function (seconds) {
+  
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
 
+import throttle from 'lodash.throttle';
+const item = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  input: document.querySelector('.feedback-form input'),
+};
+const STORAGE_KEY = 'feedba0ck-form-state';
+
+let formData = {};
+item.form.addEventListener('submit', onFormSubmit);
+item.form.addEventListener('input', throttle(onTextareaInput, 500));
+
+function onTextareaInput(evt) {
+  formData[item.textarea.name] = item.textarea.value;
+  formData[item.input.name] = item.input.value;
+  const saveDataEl = JSON.stringify(formData);
+  localStorage.setItem(STORAGE_KEY, saveDataEl);
+}
+
+const savedMessage = localStorage.getItem(STORAGE_KEY);
+const pasrsedSav = JSON.parse(savedMessage);
+
+function populateTextarea() {
+  if (savedMessage) {
+    const keys = Object.keys(pasrsedSav);
+    for (const key of keys) {
+      item.form.elements[key].value = pasrsedSav[key];
+    }
+  }
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(`${item.input.name}:`, item.input.value);
+  console.log(`${item.textarea.name}:`, item.textarea.value);
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+
+populateTextarea();
