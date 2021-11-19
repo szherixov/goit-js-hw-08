24e6146d76e8c938d29d1e16ac292b3e7a767bae
szherixov <s0p9n9k@gmail.com>
2021-11-19 15:07:00 +0200

    finalyyyyy

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,17 +4,17 @@ const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 const VIDEO_KEY = 'videoplayer-current-time';
 
-const onPlay = function (data) {
+const playMuvie = function (data) {
   const time = JSON.stringify(data.seconds);
   localStorage.setItem(VIDEO_KEY, time);
   console.log('played the video!', time);
 };
-player.on('timeupdate', throttle(onPlay, 1000));
+player.on('timeupdate', throttle(playMuvie, 1000));
 
-const saveTime = localStorage.getItem(VIDEO_KEY);
+const applicationTime = localStorage.getItem(VIDEO_KEY);
 
 player
-  .setCurrentTime(saveTime)
+  .setCurrentTime(applicationTime)
   .then(function (seconds) {
   
   })
